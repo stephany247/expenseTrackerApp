d6b31323b98c5f3310fc3be204d4f0ad993282a9
Stephanie Oguocha <stephanieoguocha@gmail.com>
2026-03-20 13:04:22 +0100

    feat: update dependencies and add new screens
    - Updated expo version from ~55.0.8 to ~54.0.0
    - Added navigation dependencies: @react-navigation/bottom-tabs, @react-navigation/native, @react-navigation/native-stack
    - Added react-native-safe-area-context and react-native-screens
    - Created AllExpenses screen
    - Created ManageExpenses screen
    - Created RecentExpenses screen

---
+++ screens/AllExpenses.js
@@ -0,0 +1,9 @@
+import { Text } from 'react-native'
+
+function AllExpenses() {
+  return (
+    <Text>AllExpenses</Text>
+  )
+}
+
+export default AllExpenses
---
+++ screens/ManageExpenses.js
@@ -0,0 +1,9 @@
+import { Text } from 'react-native'
+
+function ManageExpenses() {
+  return (
+    <Text>ManageExpenses</Text>
+  )
+}
+
+export default ManageExpenses
