3aa51a580882aa771968664d4a857e571e0ee16d
Stephanie Oguocha <stephanieoguocha@gmail.com>
2026-03-20 14:11:22 +0100

    feat: implement ExpensesOutput component with dummy data and update screens to use it

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, Text } from "react-native";
 
-function ExpensesList() {
-  return <FlatList />;
+function renderExpenseItem(itemData) {
+  return <Text>{itemData.item.description}</Text>
+}
+
+function ExpensesList({ expenses }) {
+  return <FlatList data={expenses} renderItem={renderExpenseItem} keyExtractor={(item) => item.id} />;
 }
 
 export default ExpensesList;
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,8 @@
 import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function RecentExpenses() {
-  return <Text>RecentExpenses</Text>;
+  return <ExpensesOutput expensesPeriod="Last 7 days" />;
 }
 
 export default RecentExpenses;
